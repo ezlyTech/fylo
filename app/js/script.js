b359067727715ab688b3e16fe2917c13a5af6926
Eliz Marie Manalo <tech.ezly@gmail.com>
2022-07-18 20:52:40 +0800

    general fixes

---
+++ app/js/script.js
@@ -0,0 +1,10 @@
+var splide = new Splide( '.splide', {
+    perPage: 3,
+    rewind : true,
+    wheel: true,
+    autoplay: true,
+    loop: true,
+    speed: 2000,
+  } );
+  
+splide.mount();
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-
+var splide=new Splide(".splide",{perPage:3,rewind:!0,wheel:!0,autoplay:!0,loop:!0,speed:2e3});splide.mount();
 //# sourceMappingURL=script.js.map
